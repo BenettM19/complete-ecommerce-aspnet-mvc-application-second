25bfcb7777ecc8795d29998cfd31213facee0fb3
BenettM19 <bm53089@ubt-uni.net>
2022-12-09 19:39:20 +0100

    Add project files.

---
+++ eTicketsSecond/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eTicketsSecond.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
